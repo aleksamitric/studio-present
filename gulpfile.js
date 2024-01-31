90f39849284353d55f37d9d75a584eca2d31c4e9
aleksamitric <aleksamitric.6@gmail.com>
2024-02-01 00:55:23 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,1 @@
+'use strict'; const gulp = require('gulp'); const sass = require('gulp-sass')(require('sass')); function buildStyles() { return gulp.src('./sass/**/*.scss') .pipe(sass().on('error', sass.logError)) .pipe(gulp.dest('./css')); } function watch() { gulp.watch('./sass/**/*.scss', buildStyles); } exports.buildStyles = buildStyles; exports.watch = watch;
