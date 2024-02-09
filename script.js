821859fd08ea6406a58a4f3c6a0e5081e67cab29
aleksamitric <aleksamitric.6@gmail.com>
2024-02-09 02:17:03 +0100

    gotov popup. Hamburger meni dodat, responsive za mobilne do pola uradnjen. Treba zavrsiti responsive i dodati slajdere.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var mode = document.getElementById("mode-div");
 var logos = document.querySelectorAll(".centriran_logo");
 var genussnetz = document.getElementById("genussnetz-logo");
+
 mode.onclick = function(){
     document.body.classList.toggle("light-mode");
     if (document.body.classList.contains("light-mode")){
@@ -39,4 +40,45 @@ var icon = document.querySelector('#popup-close i');
 
 icon.addEventListener('click', function() {
     popup.classList.add('hidden');
+});
+
+
+
+//Sakrivanje elemenata na <992px widht
+var hamburgerDiv = document.getElementById("nav-hamburger");
+var navIcon = document.getElementById("nav-icon");
+var logo = document.getElementById("logo");
+
+function proveriSirinuEkrana() {
+  if (window.innerWidth < 992) {
+    hamburgerDiv.classList.add("hidden");
+    navIcon.classList.remove("hidden");
+  } else {
+    hamburgerDiv.classList.remove("hidden");
+    navIcon.classList.add("hidden");
+  }
+}
+
+//Konstantna provera sirine ekrana
+function onemoguciScroll(event) {
+    event.preventDefault();
+}
+
+window.addEventListener("load", proveriSirinuEkrana);
+window.addEventListener("resize", proveriSirinuEkrana);
+
+navIcon.addEventListener('click', function() {
+    hamburgerDiv.classList.toggle("hidden");
+    document.body.classList.toggle("nav-body");
+    logo.classList.toggle("hidden");
+    if (logo.classList.contains("hidden")) {
+        window.addEventListener('wheel', onemoguciScroll, { passive: false });
+        navIcon.classList.add('fa-x');
+        navIcon.classList.remove('fa-bars');
+    } else {
+        window.removeEventListener('wheel', onemoguciScroll);
+        navIcon.classList.add('fa-bars');
+        navIcon.classList.remove('fa-x');
+    }
+
 });
